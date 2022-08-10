6c2c55e06e69f70bdd448e7fd5c8f2dd1de69230
ceilingfang <romannequin@gmail.com>
2022-08-09 19:24:37 -0700

    added js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let header = document.querySelector('#page-header')
 header.style.textAlign = "left"
 let dogImages =document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+}
 // Select some elements...
