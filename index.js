af0ff65083e6d7c8d7caa9d417af6576d9b8f599
ceilingfang <romannequin@gmail.com>
2022-08-09 19:45:30 -0700

    added js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let dogImages =document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++) {
     dogImages[i].style.borderRadius = '50px'
 }
-let dogNames = document.querySelectorAll('dog-names')
+let dogNames = document.querySelectorAll('.dog-names')
 for(let i = 0; i < dogNames.length; i++) {
     dogNames[i].style.textAlign = 'left'
 }
