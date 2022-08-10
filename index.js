abe6afefd30f7ae81109b8856c32e6764dc1c2b4
ceilingfang <romannequin@gmail.com>
2022-08-09 19:40:12 -0700

    added and fixed js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let dogImages =document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++) {
     dogImages[i].style.borderRadius = '50px'
 }
-let dogNames = document.querySelectorAll("dog-name")
-header.style.textAlign = "left"
+let dogNames = document.querySelectorAll('dog-names')
+for(let i = 0; i < dogNames.length; i++) {
+    dogNames[i].style.textAlign ='left'
+}
+
 // Select some elements...
