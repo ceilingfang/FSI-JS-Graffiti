957d4ea79c9aa9f65fcbbb76428741b554c54f13
ceilingfang <romannequin@gmail.com>
2022-08-12 13:18:34 -0700

    added js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let header = document.querySelector('#page-header')
 header.style.textAlign = "left"
-let dogImages =document.querySelectorAll('.dog-image')
+let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++) {
     dogImages[i].style.borderRadius = '50px'
 }
@@ -9,5 +9,10 @@ for(let i = 0; i < dogNames.length; i++) {
     dogNames[i].style.textAlign = 'left'
 }
 let footer = document.querySelector('.footer')
-footer.style.textColor = 'blue'
+footer.style.color = 'blue'
+
+let p = document.querySelectorAll('.dog-description')
+for(let i = 0; i < dogDescription.length; i++) {
+    dogDescription[i].style.color = 'green'
+}
 // Select some elements...
