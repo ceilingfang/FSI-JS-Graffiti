b3220781d0078774e0f9cb339ba2e8f23783e7d0
ceilingfang <romannequin@gmail.com>
2022-08-12 20:51:18 -0700

    removed js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ header.style.textAlign = "left"
 let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++) {
     dogImages[i].style.borderRadius = '50px'
-    dogImages[i].style.transform.rotate = '180deg'
 }
 
 let dogNames = document.querySelectorAll('.dog-name')
