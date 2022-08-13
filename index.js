a71d7a02e175df6a866f83a1e34a6d44045d8ba9
ceilingfang <romannequin@gmail.com>
2022-08-12 20:39:41 -0700

    added js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 let header = document.querySelector('#page-header')
 header.style.textAlign = "left"
+
 let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++) {
     dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform.rotate = '180deg'
 }
+
 let dogNames = document.querySelectorAll('.dog-name')
 for(let i = 0; i < dogNames.length; i++) {
     dogNames[i].style.textAlign = 'left'
 }
+
 let footer = document.querySelector('.footer')
 footer.style.color = 'blue'
 
-let p = document.querySelectorAll('.dog-description')
+let dogDescription = document.querySelectorAll('.dog-description')
 for(let i = 0; i < dogDescription.length; i++) {
     dogDescription[i].style.color = 'green'
 }
+
+
 // Select some elements...
